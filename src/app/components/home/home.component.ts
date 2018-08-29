5480600579f772753bb274109744653c91ce8dbd
gnegri <grantnegri@gmail.com>
2018-08-29 01:09:35 -0400

    only show most recent blog post on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BlogPost } from '../../classes/BlogPost';
+import { blogPostList } from '../../data/blog/BlogPosts';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  mostRecentPost: BlogPost = blogPostList[0];
 
   constructor() { }
 
